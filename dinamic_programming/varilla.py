da0b111f249ecad4828faaa38d969132e9cffa31
emiduran05 <emiduranf05@gmail.com>
2025-09-02 13:34:38 -0600

    Add files via upload

---
+++ dinamic_programming/varilla.py
@@ -0,0 +1,73 @@
+import math
+import time
+
+inicioR = time.time()
+
+p = [1,5,8,9,10,17,17,20,24,30] #Precios de la varilla
+varilla1 = 4
+varilla2 = 6
+varilla3 = 10
+varilla4 = 25
+
+#Solucion recursiva
+def cutRod(p,n): #p = precio de las varillas, y n = longitud de la misma
+    if n == 0:
+        return 0
+    q = -(math.inf)
+    for i in range(1, min(n, len(p)) + 1):
+        q = max(q, p[i - 1] + cutRod(p, n - i))
+    return q
+
+
+print("Solucion recursiva: ", cutRod(p,varilla4))
+
+finR = time.time()
+print("Tiempo de ejecución recursivo", finR-inicioR, " segundos")
+#Solución con programación dinamica
+
+inicioDP = time.time()
+
+memo = [-1] * (varilla4 + 1)
+def cutRodDP(p,n,memo):
+    
+    if memo[n] == -1:
+        if n == 0:
+            memo[n] = 0
+            return 0 
+        q = -(math.inf)
+        for i in range(1, min(n, len(p)) + 1):
+            q = max(q, p[i -1] + cutRodDP(p, n - i, memo))
+        memo[n] = q
+    return memo[n]
+
+
+print("Solución con programación dinamica dinamica: ", cutRodDP(p,varilla4,memo))
+
+finDP = time.time()
+
+print("Tiempo de ejecución con programacion dinamica: ", finDP - inicioDP, " segundos")
+
+#Solucion bottom-up
+
+inicioBU = time.time()
+
+def cutRodBottomUp(p,n):
+    
+    dp = [0] * (n+1)
+    
+    for i in range(1,n+1):
+        q = -(math.inf)
+        
+        for j in range(1, i + 1):
+            if(j <= len(p)):
+                q = max(q, p[j - 1] + dp[i - j])
+            dp[i] = q
+    
+    return dp[n]
+
+
+print("Solucion con Bottom Up: ", cutRodBottomUp(p,varilla4))
+
+finBU = time.time()
+
+print("Tiempo de ejecución con Bottom Up: ", finBU - inicioBU, " segundos")
